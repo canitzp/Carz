93a142a2286e624dfad57010bbeea950ccd0fb51
canitzp <canitzp@googlemail.com>
2017-08-10 23:03:18 +0200

    It does render something, I think

--- src/main/java/de/canitzp/carz/entity/EntityCar.java
+++ src/main/java/de/canitzp/carz/entity/EntityCar.java
@@ -1,12 +1,17 @@
 package de.canitzp.carz.entity;
 
+import de.canitzp.carz.entity.renderer.RenderCar;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 /**
  * @author canitzp
@@ -33,17 +38,50 @@ public class EntityCar extends Entity {
 
     }
 
-    @SideOnly(Side.CLIENT)
-    public void renderCar(double x, double y, double z, float entityYaw, float partialTicks){
-
-    }
-
+    @Override
     public boolean processInitialInteract(EntityPlayer player, EnumHand hand) {
         if (!this.world.isRemote && !player.isSneaking()) {
             player.startRiding(this);
             return true;
         }
         return false;
+    }
+
+    /**
+     * Returns a boundingBox used to collide the entity with other entities and blocks. This enables the entity to be
+     * pushable on contact, like boats or minecarts.
+     */
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBox(Entity entityIn) {
+        return entityIn.canBePushed() ? entityIn.getEntityBoundingBox() : null;
+    }
+
+    /**
+     * Returns the collision bounding box for this entity
+     */
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox() {
+        return this.getEntityBoundingBox();
+    }
+
+    @Override
+    public boolean canBeCollidedWith() {
+        return !this.isDead;
+    }
+
+    /**
+     * Returns true if this entity should push and be pushed by other entities when colliding.
+     */
+    @Override
+    public boolean canBePushed() {
+        return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void renderCar(double x, double y, double z, float entityYaw, float partialTicks) {
+        RenderCar.MODEL_CAR.render(this, partialTicks, 0, -0.1F, 0, 0, 0.0625F);
     }
 
 }
--- src/main/java/de/canitzp/carz/entity/renderer/RenderCar.java
+++ src/main/java/de/canitzp/carz/entity/renderer/RenderCar.java
@@ -1,6 +1,7 @@
 package de.canitzp.carz.entity.renderer;
 
 import de.canitzp.carz.entity.EntityCar;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -13,6 +14,8 @@ import javax.annotation.Nullable;
  */
 public class RenderCar extends Render<EntityCar> {
 
+    public static final ModelCar MODEL_CAR = new ModelCar();
+
     public RenderCar(RenderManager renderManager) {
         super(renderManager);
     }
@@ -20,12 +23,32 @@ public class RenderCar extends Render<EntityCar> {
     @Nullable
     @Override
     protected ResourceLocation getEntityTexture(EntityCar entity) {
+        //return new ResourceLocation("textures/entity/boat/boat_oak.png");
         return null;
     }
 
     @Override
     public void doRender(EntityCar car, double x, double y, double z, float entityYaw, float partialTicks) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
+        //this.setupRotation(entity, entityYaw, partialTicks);
+        this.bindEntityTexture(car);
+
+        if (this.renderOutlines)
+        {
+            GlStateManager.enableColorMaterial();
+            GlStateManager.enableOutlineMode(this.getTeamColor(car));
+        }
+
         car.renderCar(x, y, z, entityYaw, partialTicks);
+
+        if (this.renderOutlines)
+        {
+            GlStateManager.disableOutlineMode();
+            GlStateManager.disableColorMaterial();
+        }
+
+        GlStateManager.popMatrix();
         super.doRender(car, x, y, z, entityYaw, partialTicks);
     }
 }
