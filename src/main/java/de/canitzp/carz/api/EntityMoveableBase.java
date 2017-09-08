3399f00bb93a1563684292c2f52a1a2200427910
Alexander Lüpges/MisterErwin <git@askarian.net>
2017-09-08 20:34:24 +0200

    Now able to drive up slopes

--- src/main/java/de/canitzp/carz/api/EntityMoveableBase.java
+++ src/main/java/de/canitzp/carz/api/EntityMoveableBase.java
@@ -51,7 +51,7 @@ public abstract class EntityMoveableBase extends EntityPartedBase /*EntityCollid
     public void onUpdate() {
         super.onUpdate();
         this.onUpdate(this.canPassengerSteer());
-        this.stepHeight = 0.2f; //Yeah - config
+        this.stepHeight = 0.4f; //Yeah - config
 
         if (this.isCollidedHorizontally) {
             blockCollisionCheck();
