40d08c228dd5e5434a74e476ba2df5e8760c03ad
canitzp <canitzp@googlemail.com>
2017-11-02 22:52:59 +0100

    Fixed basic blocks Finished implementing of the pylon european some localisations

--- src/main/java/de/canitzp/carz/blocks/BlockBasic.java
+++ src/main/java/de/canitzp/carz/blocks/BlockBasic.java
@@ -11,6 +11,7 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.IStateMapper;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
@@ -30,44 +31,50 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author canitzp
  */
 public class BlockBasic extends BlockBase<BlockBasic> {
 
-    public static final PropertyEnum<EnumBasicBlocks> TYPE = PropertyEnum.create("type", EnumBasicBlocks.class);
+    private EnumBasicBlocks type;
 
-    public BlockBasic() {
+    public BlockBasic(EnumBasicBlocks type) {
         super(Material.ROCK);
-        this.setRegistryName(Carz.MODID, "basic");
-        this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, EnumBasicBlocks.PYLON_EUROPEAN));
+        this.type = type;
+        this.setRegistryName(Carz.MODID, "basic_" + type.getName().toLowerCase());
     }
 
     @SideOnly(Side.CLIENT)
     @Override
     public void registerClient() {
-        for(EnumBasicBlocks block : EnumBasicBlocks.values()){
-            ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(this.getRegistryName(), TYPE.getName() + "=" + block.getName()));
-        }
+        ModelResourceLocation loc = new ModelResourceLocation(new ResourceLocation(Carz.MODID, "basic"), "type=" + type.getName());
+        ModelLoader.setCustomStateMapper(this, block -> {
+            Map<IBlockState, ModelResourceLocation> map = new HashMap<>();
+            map.put(block.getDefaultState(), loc);
+            return map;
+        });
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, loc);
     }
 
     @Nonnull
     @Override
     public Material getMaterial(IBlockState state) {
-        return state.getValue(TYPE).getMaterial(state);
+        return type.getMaterial(state);
     }
 
     @Nonnull
     @Override
     public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        return state.getValue(TYPE).getColor(state, worldIn, pos);
+        return type.getColor(state, worldIn, pos);
     }
 
     @Override
     public boolean isOpaqueCube(IBlockState state) {
-        return state.getValue(TYPE).isFullBlock(state);
+        return type != null ? type.isFullBlock(state) : super.isOpaqueCube(state);
     }
 
     @Override
@@ -77,72 +84,14 @@ public class BlockBasic extends BlockBase<BlockBasic> {
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return state.getValue(TYPE).getBoundingBox(state, source, pos);
+        return type.getBoundingBox(state, source, pos);
     }
 
     @Override
     public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_) {
-        for(AxisAlignedBB hitBox : state.getValue(TYPE).getHitBoxes(state, worldIn, pos, entityBox, collidingBoxes, entityIn, p_185477_7_)){
+        for(AxisAlignedBB hitBox : type.getHitBoxes(state, worldIn, pos, entityBox, collidingBoxes, entityIn, p_185477_7_)){
             addCollisionBoxToList(pos, entityBox, collidingBoxes, hitBox);
         }
     }
 
-    @Nonnull
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(TYPE, EnumBasicBlocks.values()[meta]);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(TYPE).ordinal();
-    }
-
-    @Nonnull
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, TYPE);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list){
-        for(EnumBasicBlocks basic : EnumBasicBlocks.values()){
-            ItemStack stack = new ItemStack(this);
-            NBTTagCompound nbt = new NBTTagCompound();
-            nbt.setShort("BlockIndex", (short) basic.ordinal());
-            stack.setTagCompound(nbt);
-            list.add(stack);
-        }
-    }
-
-    @Nonnull
-    @Override
-    public String getUnlocalizedName() {
-        return super.getUnlocalizedName();
-    }
-
-    @Override
-    public ItemBlock getItemBlock() {
-        return new ItemBlockBasic(this);
-    }
-
-    public static class ItemBlockBasic extends ItemBlockCustom{
-
-        public ItemBlockBasic(BlockBase block) {
-            super(block);
-            this.setRegistryName(block.getRegistryName());
-        }
-
-        @Nonnull
-        @Override
-        public String getUnlocalizedName(ItemStack stack) {
-            NBTTagCompound nbt = stack.getTagCompound();
-            if(nbt != null && nbt.hasKey("BlockIndex", Constants.NBT.TAG_SHORT)){
-                EnumBasicBlocks index = EnumBasicBlocks.values()[nbt.getShort("BlockIndex")];
-                return super.getUnlocalizedName(stack) + "_" + index.getName().toLowerCase();
-            }
-            return super.getUnlocalizedName(stack);
-        }
-    }
 }
