1000b3ea1b77c1d25b8a375966ec382c4778f3e3
canitzp <canitzp@googlemail.com>
2017-11-16 00:05:20 +0100

    Fixed a dependency error

--- src/main/java/de/canitzp/carz/util/BlockProps.java
+++ src/main/java/de/canitzp/carz/util/BlockProps.java
@@ -1,7 +1,6 @@
 package de.canitzp.carz.util;
 
 import de.canitzp.carz.client.PixelMesh;
-import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
@@ -9,7 +8,6 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.property.IUnlistedProperty;
 import net.minecraftforge.common.property.Properties;
-import org.cyclops.cyclopscore.block.property.UnlistedProperty;
 
 /**
  * @author canitzp
@@ -20,7 +18,27 @@ public class BlockProps {
     public static final PropertyBool BOTTOM = PropertyBool.create("bottom");
     public static final PropertyInteger SLOPE_NUMBER = PropertyInteger.create("slope_number", 0, 3);
 
-    public static final IUnlistedProperty<PixelMesh> MODEL = new UnlistedProperty<>("model_id", PixelMesh.class);
+    public static final IUnlistedProperty<PixelMesh> MODEL = new IUnlistedProperty<PixelMesh>() {
+        @Override
+        public String getName() {
+            return "model_id";
+        }
+
+        @Override
+        public boolean isValid(PixelMesh value) {
+            return value != null;
+        }
+
+        @Override
+        public Class<PixelMesh> getType() {
+            return PixelMesh.class;
+        }
+
+        @Override
+        public String valueToString(PixelMesh value) {
+            return value.toString();
+        }
+    };
     public static final IUnlistedProperty<EnumFacing> FACING_MESH =
             new Properties.PropertyAdapter<>(PropertyEnum.create("facing", EnumFacing.class));
 }
